d871f46a3a2a217f45294668fc26ec73d72d6817
Dmitrii-Gor <90476384+Dmitrii-Gor@users.noreply.github.com>
2023-04-19 20:59:31 +0300

    Delete django_3hw directory

--- django_3hw/phones/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Phone
-
-
-@admin.register(Phone)
-class PhoneAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price', 'image', 'release_date', 'lte_exists']
-    list_filter = ['name', 'price', 'lte_exists']
-    prepopulated_fields = {'slug': ('name',)}
